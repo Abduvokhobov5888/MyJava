edecd1ba3f4f6451f0bd45d28cb191fa7d997b7d
Abbosbek <vip.real.aa@gmail.com>
2022-06-28 15:34:53 +0500

    hashtag

--- app/src/main/java/com/example/myjava/MainActivity.java
+++ app/src/main/java/com/example/myjava/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.PrecomputedText;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -20,11 +21,12 @@ public class MainActivity extends AppCompatActivity {
     }
     void initViews(){
         TextView textView = findViewById(R.id.textView);
-        String text = "I would like to do something similar to the https://twitter.com app";
+        String text = "I would #like to do #something similar to the #Twitter app";
 
         Spannable spannable = new SpannableString(text);
-        spannable.setSpan(new ForegroundColorSpan(Color.BLUE),44,63, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
+        spannable.setSpan(new ForegroundColorSpan(Color.GREEN),8, 13, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(new ForegroundColorSpan(Color.GREEN),20,30,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(new ForegroundColorSpan(Color.GREEN),46,54,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         textView.setText(spannable);
     }
 }
